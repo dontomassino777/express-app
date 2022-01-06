9cf036fdcbde08abf744588ee76d4ca62d2271e1
Tomas Fisher <dontomassino777@gmail.com>
2022-01-06 11:15:49 -0700

    npm start scripts and env port variables set up

--- app.js
+++ app.js
@@ -2,15 +2,18 @@ const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
 
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public/')));
 
 app.get('/', (req, res) => {
     res.send('Hello from my app');
 });
 
-app.listen(3000, () => {
-    debug(`Jammin' on port ${chalk.green('3000')}`);
+app.listen(PORT, () => {
+    debug(`Jammin' on port ${chalk.green(PORT)}`);
 });
