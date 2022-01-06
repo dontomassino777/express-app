857b0456372f2379f61b9d527f34bfac5ac92e00
Tomas Fisher <dontomassino777@gmail.com>
2022-01-06 10:55:14 -0700

    express server setup

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('tiny'));
+
+app.get('/', (req, res) => {
+    res.send('Hello from my app');
+});
+
+app.listen(3000, () => {
+    debug(`Jammin' on port ${chalk.green('3000')}`);
+});
